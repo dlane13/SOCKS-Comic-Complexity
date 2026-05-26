05b2a5f6300a78df5b1aa547c31c41b4a1bb09df
dlane13 <2023del@gmail.com>
2026-05-26 16:34:23 -0400

    moved env out of repo

--- num_panels.py
+++ num_panels.py
@@ -3,17 +3,22 @@ import pandas as pd
 from pathlib import Path
 
 #set data directory
-images_path = Path('~/Library/CloudStorage/OneDrive - University of Vermont/VEA study - tasks for students/SOCKS project/CH Corpus/Originals')
-data_path = '~/Library/CloudStorage/OneDrive - University of Vermont/VEA study - tasks for students/SOCKS project/CH Corpus/CHB_corpus_master.xlsx' #finish path to excel spreadsheet
+images_path = Path('/Users/darbylane/Library/CloudStorage/OneDrive-UniversityofVermont/VEA study - tasks for students/SOCKS project/CH Corpus/Originals')
+data_path = '/Users/darbylane/Library/CloudStorage/OneDrive-UniversityofVermont/VEA study - tasks for students/SOCKS project/CH Corpus/CHB_corpus_master.xlsx' #finish path to excel spreadsheet
 
 #extract num_panels from data_path
-num_panels = pd.read_excel(data_path, usecols='Panel #')
+#TODO: not working, not sure why not - doesn't think "Panel #" is a column name
+num_panels = pd.read_excel(data_path, usecols="Panel #")
+
+print(list(num_panels.columns.values))
+
+#print(num_panels[1])
 
 for comic_num in len(images_path.iterdir()):
     #pair comics with num_panels
-
+    continue
     #use CNN for embedding
-    
+
     #what backbone for output?
 
     #MSE for optimization
